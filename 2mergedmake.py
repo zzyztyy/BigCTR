aeb5a028750a26a7ec5d38307d41dab49d20f41d
zzyztyy <2375672032@qq.com>
2017-11-05 21:23:30 +0800

    nothing

--- 2mergedmake.py
+++ 2mergedmake.py
@@ -20,19 +20,21 @@ def getoneorb(text, startline):
     chalist = []
     roclist = []
     a = text[startline].split()
-    # print(a)
+    print(a)
     lenth1 = int(a[1])
-    b = text[startline+lenth1+1].split()
-    # print(b)
+    b = text[startline + lenth1 + 2].split()
+    print(b)
     lenth2 = int(b[1])
-    for i in range(startline+2, startline+lenth1+1):
+    for i in range(startline + 2, startline + lenth1 + 2):
+        # print(text[i])
         chalist.append(text[i])
-    for i in range(startline+lenth1+3, startline+lenth1+lenth2+3):
+    for i in range(startline + lenth1 + 4, startline + lenth1 + lenth2 + 4):
         roclist.append(text[i])
-    return startline+lenth1+lenth2+3, chalist, roclist
+        # print(text[i])
+    return startline + lenth1 + lenth2 + 4, chalist, roclist
 
 #GPS0 yy1 mm2 dd3 hh4 mm5 ss6 radius7 lat8 lon9 den10 (Temperature)
-#lat lon dlt lgN Vpm
+# UT0 LT1 VperM2 LogN3 DipLat4
 def draw(cha, roc):
     plt.figure(figsize=[10, 10], dpi=300)
     plt.subplots_adjust(hspace=0.1, left=0.1, bottom=0.05, right=0.97, top=0.97)
@@ -47,33 +49,33 @@ def draw(cha, roc):
         lat.append(float(a[8]))
     for i in range(len(roc)):
         b = roc[i].split()
-        latr.append(float(b[0]))
+        latr.append(float(b[4]))
         lgN.append(float(b[3]))
-        Vpm.append(float(b[4]))
+        Vpm.append(float(b[2]))
     bf.sort2(lgN, latr)
     bf.sort2(Vpm, latr)
     bf.sort2(latr, latr)
-    rhs = np.array(latr).min()
-    lhs = np.array(latr).max()
-    lat2 = np.linspace(rhs, lhs, int((lhs-rhs)/0.05))
-    Vpmfun = interp1d(latr, Vpm)
-    lgNfun = interp1d(latr, lgN)
-    Vpm2 = []
-    lgN2 = []
-    for i in range(len(lat2)):
-        Vpm2.append(Vpmfun(lat2[i]))
-        lgN2.append(lgNfun(lat2[i]))
-    Vpm2 = bf.smooth(Vpm2, 60)
-    lgN2 = bf.smooth(lgN2, 60)
+    # rhs = np.array(latr).min()
+    # lhs = np.array(latr).max()
+    # lat2 = np.linspace(rhs, lhs, int((lhs-rhs)/0.05))
+    # Vpmfun = interp1d(latr, Vpm)
+    # lgNfun = interp1d(latr, lgN)
+    # Vpm2 = []
+    # lgN2 = []
+    # for i in range(len(latr)):
+    #     Vpm2.append(Vpmfun(lat2[i]))
+    #     lgN2.append(lgNfun(lat2[i]))
+    Vpm2 = bf.smooth(Vpm, 60)
+    lgN2 = bf.smooth(lgN, 60)
     plt.subplot(3, 1, 1)
     plt.scatter(latr, lgN, s=0.1, c='k')
-    plt.plot(lat2, lgN2, linewidth=1, c='r')
+    plt.plot(latr, lgN2, linewidth=1, c='r')
     # plt.axis([-50, 50, 3, 9])
     plt.xlim(-20, 20)
     plt.ylim(4.5, 6.5)
     plt.subplot(3, 1, 2)
     plt.scatter(latr, Vpm, s=0.1, c='k')
-    plt.plot(lat2, Vpm2, linewidth=1, c='r')
+    plt.plot(latr, Vpm2, linewidth=1, c='r')
     # plt.axis([-50, 50, -150, 150])
     plt.xlim(-20, 20)
     plt.ylim(-75, 75)
@@ -84,9 +86,9 @@ def draw(cha, roc):
     # plt.show()
 
 def daydraw(name):
-    path = 'mergedOrbqd\\'
+    # path = 'mergedOrbqd\\'
     # name = '20011122.txt'
-    data = readfile(path+name)
+    data = readfile(name)
     nextline = 0
     temp = 0
     while nextline < len(data):
@@ -107,6 +109,7 @@ def daystatic():
     files = os.listdir('mergedOrbqd')
     ctr = []
     vpm = []
+    pbw = []
     alllt = []
     for file in files:
         path = 'mergedOrbqd\\'
@@ -120,7 +123,9 @@ def daystatic():
             alllt.append(lt)
             ctr.append(np.log10(getCTR(cha)))
             vpm.append(getVpm(roc))
-    plt.scatter(ctr, vpm, c=alllt, cmap='brg', s=1)
+            pbw.append(np.log10(getPBwidth(roc)))
+    plt.scatter(ctr, vpm, c=alllt, cmap='brg', s=2, vmax=23, vmin=17)
+    plt.title('lgCTR-Vpm')
     plt.colorbar()
     plt.show()
 
@@ -138,6 +143,7 @@ def getCTR(cha):
         elif lat >10 and lat<20:
             norpeak.append(float(a[10]))
     ctr= (np.array(soupeak).mean()+np.array(norpeak).mean())/(2*np.array(valley).mean())
+    # ctr = 1./np.array(valley).mean()
     return max(ctr, 1)
 
 def getVpm(roc):
@@ -145,16 +151,47 @@ def getVpm(roc):
     for i in range(len(roc)):
         a = roc[i].split()
         lat = float(a[0])
-        if lat >-5 and lat<5:
+        if lat > -10 and lat < 10:
             Vpm.append(float(a[4]))
     return np.array(Vpm).mean()
+
+
+def getPBwidth(roc):
+    lgN = []
+    latr = []
+    for i in range(len(roc)):
+        a = roc[i].split()
+        lat = float(a[0])
+        if lat > -20 and lat < 20:
+            lgN.append(float(a[3]))
+            latr.append(lat)
+    bf.sort2(lgN, latr)
+    bf.sort2(latr, latr)
+    rhs = np.array(latr).min()
+    lhs = np.array(latr).max()
+    lat2 = np.linspace(rhs, lhs, int((lhs - rhs) / 0.05))
+    lgNfun = interp1d(latr, lgN)
+    lgN2 = []
+    for i in range(len(lat2)):
+        lgN2.append(lgNfun(lat2[i]))
+    lgN3 = bf.smooth(lgN2, 60)
+    if len(lgN2) > 60:
+        dlgN = np.array(lgN3) - np.array(lgN2)
+    else:
+        dlgN = np.ones(len(lgN2))
+    temp = 0
+    for i in range(len(dlgN)):
+        # if abs(dlgN[i]) > 0.2:
+        temp = temp + abs(dlgN[i])
+    return temp / len(latr)
+
 
 
 if __name__ == '__main__':
     # files = os.listdir('mergedOrbqd')
     # for file in files:
     #     try:
-    #         daydraw(file)
+    # daydraw('mergedqd.txt')
     #     except:
     #         print(file)
     daystatic()
